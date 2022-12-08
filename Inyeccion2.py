1b5e0e7086747cf7ba35c52f7c19a6a7c7a0d1ea
Andy <72534502+Sarri09@users.noreply.github.com>
2022-12-08 04:02:35 -0300

    Update Inyeccion2.py

--- Inyeccion2.py
+++ Inyeccion2.py
@@ -9,7 +9,3 @@ for i in range(0, 10):
              op='is-at'), 
         iface='lo', verbose=False)
     print("sent packet")
-
-
-
-    ##send(ARP(pdst='172.17.0.2',psrc='172.17.0.3',hwdst='02:42:ac:11:00:02',op=2),iface='lo',verbose=False)
